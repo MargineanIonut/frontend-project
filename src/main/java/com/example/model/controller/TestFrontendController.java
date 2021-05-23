2b0f12f0293d917902b3ddde78063b8ca85aaf36
MargineanIonut <ionut.marginean01@gmail.com>
2021-05-23 16:45:04 +0200

    --

--- src/main/java/com/example/model/controller/TestFrontendController.java
+++ src/main/java/com/example/model/controller/TestFrontendController.java
@@ -2,11 +2,14 @@ package com.example.model.controller;
 
 import com.example.model.ProductDto;
 import com.example.model.client.DatabaseClient;
+import com.example.model.client.StorageClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +20,20 @@ public class TestFrontendController {
     @Autowired
     DatabaseClient databaseClient;
 
+    @Autowired
+    StorageClient storageClient;
+
+
+    @GetMapping("")
+    public String test(Model model){
+        model.addAttribute("h1","This is a title");
+        model.addAttribute("numberOfSlides",10);
+        return "home";
+    }
+
+    void readFile(String filePath) throws FileNotFoundException {
+
+    }
 
     @GetMapping("/getAllProducts")
     public String getAllProducts(Model model){
